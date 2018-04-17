4be7f4703ac0431fbe11256dc6816b11c9cda003
Sherwood Callaway <shcallaway@gmail.com>
2018-04-17 13:48:13 -0700

    Split models and apis into their own files

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptNodeClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptNodeClientCodegen.java
@@ -14,11 +14,44 @@ public class TypeScriptNodeClientCodegen extends AbstractTypeScriptClientCodegen
         return "Generates a TypeScript nodejs client library.";
     }
 
+    @Override
+    public String apiFileFolder() {
+        return outputFolder + "/apis/" + apiPackage().replace('.', File.separatorChar);
+    }
+
+    @Override
+    public String modelFileFolder() {
+        return outputFolder + "/models/" + modelPackage().replace('.', File.separatorChar);
+    }
+
+    @Override
+    public String toModelFilename(String name) {
+        return toConventionalFilename(name);
+    }
+
+    @Override
+    public String toApiFilename(String name) {
+        return toConventionalFilename(name + "_api");
+    }
+
+    private String toConventionalFilename(String name) {
+        return name.toLowerCase().replaceAll("[_]", "-");
+    }
+    
+    @Override
+    public CodegenModel fromModel(String name, Model model, Map<String, Model> allDefinitions) {
+        CodegenModel m = super.fromModel(name, model, allDefinitions);
+        m.classFileName = toConventionalFilename(m.classFileName);
+        return m;
+    }
+
     public TypeScriptNodeClientCodegen() {
         super();
         outputFolder = "generated-code/typescript-node";
         embeddedTemplateDir = templateDir = "TypeScript-node";
-        supportingFiles.add(new SupportingFile("api.mustache", null, "api.ts"));
+        apiTemplateFiles.put("api.mustache", ".ts");
+        modelTemplateFiles.put("model.mustache", ".ts");
+        supportingFiles.add(new SupportingFile("apis.mustache", null, "apis.ts"));
+        supportingFiles.add(new SupportingFile("models.mustache", null, "models.ts"));
     }
-
 }
