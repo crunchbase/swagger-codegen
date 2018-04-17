0004e11d851d035ae3f2e4eb4f1f2ecd9b5b6775
Sherwood Callaway <shcallaway@gmail.com>
2018-04-17 15:59:01 -0700

    Fix array types; Change models to interfaces

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptNodeClientCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/TypeScriptNodeClientCodegen.java
@@ -1,6 +1,7 @@
 package io.swagger.codegen.languages;
 
 import io.swagger.codegen.SupportingFile;
+import io.swagger.models.properties.*;
 
 public class TypeScriptNodeClientCodegen extends AbstractTypeScriptClientCodegen {
 
@@ -12,6 +13,27 @@ public class TypeScriptNodeClientCodegen extends AbstractTypeScriptClientCodegen
     @Override
     public String getHelp() {
         return "Generates a TypeScript nodejs client library.";
+    }
+
+    @Override
+    public String toInstantiationType(Property p) {
+        if (p instanceof MapProperty) {
+            MapProperty ap = (MapProperty) p;
+            Property additionalProperties2 = ap.getAdditionalProperties();
+            String type = additionalProperties2.getType();
+            if (null == type) {
+                LOGGER.error("No Type defined for Additional Property " + additionalProperties2 + "\n" //
+                      + "\tIn Property: " + p);
+            }
+            String inner = getSwaggerType(additionalProperties2);
+            return instantiationTypes.get("map") + "<String, " + inner + ">";
+        } else if (p instanceof ArrayProperty) {
+            ArrayProperty ap = (ArrayProperty) p;
+            String inner = getSwaggerType(ap.getItems());
+            return instantiationTypes.get("array") + "<" + toModelName(inner) + ">";
+        } else {
+            return null;
+        }
     }
 
     public TypeScriptNodeClientCodegen() {
